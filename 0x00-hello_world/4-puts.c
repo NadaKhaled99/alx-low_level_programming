351770745c8b0b245de3cfcfa01a4220303fcfe8
Nada <nadafarrag619@gmail.com>
2023-08-12 03:23:30 +0300

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main -> main function - entry point
+ *the puts -->function used for writing string
+ *return ->should be zero and always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
