fa201beae9ffd6c15cdcb5da7793e2399442f3ec
Nada <nadafarrag619@gmail.com>
2023-08-12 03:47:52 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  *main -> main function - entry point
- *the puts ->function used for writing string
- *return value of 'main ->Always 0 (Succes)used to indicate how the program exited
+ *puts ->function used for writing string
+ *return value of 'main ->Always 0 (Succes)used to indicate how the program exit
  */
 int main(void)
 {
