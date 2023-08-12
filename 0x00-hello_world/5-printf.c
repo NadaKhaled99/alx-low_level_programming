00728b1e0fe7942cf0383da6e68c4a7e1d22a11b
Nada <nadafarrag619@gmail.com>
2023-08-12 04:02:22 +0300

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *mai ->>entry point
+ *printf-function->>used to print the display 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
