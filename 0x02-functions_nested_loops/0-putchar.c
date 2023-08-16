dc7e59784ae4a351d5bdf80b55b2e31562b33b74
Nada <nadafarrag619@gmail.com>
2023-08-16 04:03:13 +0300

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  4  * _putchar - writes the character c to stdout
+  5  * @c: The character to print
+  6  *
+  7  * Return: On success 1.
+  8  * On error, -1 is returned, and errno is set appropriately.
+  9  */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}	
