cd3af82f8e5d02d61fed23cecdfdcc27a7b324a6
Nada <nadafarrag619@gmail.com>
2023-08-17 03:00:00 +0300

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+char message;
+for (message = 'a'; message <= 'z'; message++)
+    _putchar(message);
+_putchar('\n');
+}
