d1e50be5e00e2991f8ce0f8573e705696c60b82e
Nada <nadafarrag619@gmail.com>
2023-08-17 03:30:03 +0300

    4-isalpha.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,12 @@
 #include "main.h"
+/**
+ * main - check the code.
+ * Return: Always 0.
+ */
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-		return (1);
-	else
-		return (0);
+if (c >= 'a' && c <= 'z')
+   return (1);
+else
+   return (0);
 }
