287c466d61fc89bafb4d37dd505a917add3cca93
Nada <nadafarrag619@gmail.com>
2023-08-18 03:23:14 +0300

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*_isupper -checks for uppercase character
+*@c:char to apply
+*Return: 0 Always
+*/
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
