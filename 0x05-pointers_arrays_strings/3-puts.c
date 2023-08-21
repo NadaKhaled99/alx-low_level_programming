d408a975777e3d8e44d37be8b2c3dd6d427e900f
Nada <nadafarrag619@gmail.com>
2023-08-21 23:14:48 +0300

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
-*@s:stringtoprint  
+*@str:stringtoprint
 *_puts-print string
 *Return:nothing
 */
 void _puts(char *str)
 {
 for (; *str != '\0'; str++)
- {
-   _putchar(*str);
- }
+{
+_putchar(*str);
+}
 _putchar('\n');
 }
