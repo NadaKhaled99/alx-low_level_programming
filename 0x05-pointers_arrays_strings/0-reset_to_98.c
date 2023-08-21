3717938c69a23a9faca7d67857fdfc2d7167c090
Nada <nadafarrag619@gmail.com>
2023-08-21 16:48:40 +0300

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+/**
+*reset_to_98 project - take a pointer to an int and 
+*update the value that points to be 98
+*@n: an integer input ptr
+*Return : nothing
+*/
+void reset_to_98(int *n)
+{
+    *n=98;   
+}
