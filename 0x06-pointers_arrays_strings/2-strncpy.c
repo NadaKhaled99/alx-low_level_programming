c157ee6a72990da57d1326ed378fd3109819d2ee
Nada <nadafarrag619@gmail.com>
2023-08-25 00:16:48 +0300

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*_strcat-function returns dest, the pointer to the destination string
+*_strncpy-function returns dest, the pointer to the destination string
 *@dest:This is a pointer to the destination array
 *@src: the string to be appended-This should not overlap the dest
 *@n:maxi number of bytes to cp from scr
@@ -8,9 +8,9 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-int idx = 0;
-for (idx = 0; scr[idx] && idx < n; idx++)
-dest[idx] = scr[idx];
+int idx;
+for (idx = 0; idx < n && src[idx] != '\0'; idx++)
+dest[idx] = src[idx];
 while (idx < n)
 dest[idx++] = '\0';
 return (dest);
