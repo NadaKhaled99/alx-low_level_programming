a11c4cc275a49ca5694b297dfc7a206a45782f37
Nada <nadafarrag619@gmail.com>
2023-08-31 00:30:30 +0300

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,7 +1,40 @@
-#include "main.h"
-#include "putchar.h"
-int try_palindrome(char *s, int i, int c);
+#include <stdio.h>
+int try_palindrome(char *str, int l, int c);
 int _strc_recursion(char *s);
+/**
+*_strc_recursion-function return lenght of string
+*@s: string to know lenght
+*Return:lenght of string
+*/
+
+int _strc_recursion(char *s)
+{
+if (*s)
+{
+s++;
+return (1 + _strc_recursion(s));
+}
+return (0);
+}
+/**
+*try_palindrome-function check the character for checking
+*@str:string
+*@c:iteration/counter
+*@l:string length
+*Return: 1 if string is palindrome, 0 if not
+*/
+int try_palindrome(char *str, int l, int c)
+{
+if (c >=  l)
+{
+return (1);
+}
+if (str[l] == str[c])
+{
+return (try_palindrome(str, l - 1, c + 1));
+}
+return (0);
+}
 /**
 *is_palindrome-function returns 1 if a string is a palindrome and 0 if not
 *@s:string
@@ -9,40 +42,7 @@ int _strc_recursion(char *s);
 */
 int is_palindrome(char *s)
 {
-if (*s == 0)
-{
-return (1);
-}
-return (try_palindrome(s, 0, _strc_recursion(s)));
-}
-/**
-*_strc_recursion-function return lenght of string
-*@s: string to know lenght 
-*Return:lenght of string
-*/
-int _strc_recursion(char *s)
-{
-if (*s == '\0')
-{
-return (0);
-}
-return (1 + _strc_recursion(s + 1));
-}
-/**
-*try_palindrome-function check the character 
-*@s:string
-*@i:iteration
-*@c:string length
-*/
-int try_palindrome(char *s, int i, int c)
-{
-if (*(s +i) != *(s + c - 1))
-{
-return (0);
-}
-if (i >= c)
-{
-return (1);
-}
-return (try_palindrome(s, 0, c - 1));
+int l = _strc_recursion(s);
+int c = 0;
+return (try_palindrome(s, l - 1, c));
 }
