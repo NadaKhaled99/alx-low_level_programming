3035104c289ea6d39dbb5db6b7b9d8db36f7277f
Nada <nadafarrag619@gmail.com>
2023-08-30 20:06:18 +0300

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,13 +7,13 @@
 */
 int _sqrt_recursion(int n)
 {
+ int start = 1;
+ int end = n;
+ int r = -1;
 if (n == 0 || n == 1)
 {
 return (n);
 }
-int start = 1;
-int end = n;
-int r;
 while (start <= end)
 {
 int mid = (start +  end) / 2;
@@ -24,12 +24,12 @@ return (mid);
 if (mid * mid < n)
 {
 start = mid + 1;
-result = mid;
+r = mid;
 }
 else
 {
 end = mid - 1;
 }
 }
-return (result);
+return (r);
 }
