36bc1806f2d69261fe7e97a8dd35bc1986ff53c0
Nada <nadafarrag619@gmail.com>
2023-09-02 02:07:46 +0300

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*main-multiplies two numbers.
+*@argc:the number of arguments
+*@argv:the array of arguments
+*Return:Always 0 (success)
+*/
+int main(int argc, char *argv[])
+{
+int Result = 0;
+if (argc <  2)
+{
+printf("Error\n");
+return (1);
+}
+Result = (int) *argv[1] * (int) *argv[2];
+printf("%d\n", Result);
+return (0);
+}
