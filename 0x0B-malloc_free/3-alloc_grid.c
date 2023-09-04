a4e4fdbbd896ac9170362754352b7aad3991466e
Nada <nadafarrag619@gmail.com>
2023-09-04 23:08:28 +0300

    3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*alloc_grid-function that returns a pointer to a 2D array of integers
+*@width:element
+*@height:element
+*Return: return NULL If width or height is 0 or negative,
+*/
+int **alloc_grid(int width, int height)
+{
+int **TWD;
+int hheight;
+int wwidth;
+if (height <= 0 || width <= 0)
+{
+return (NULL);
+}
+TWD = malloc(sizeof(int *) * height);
+if (TWD == NULL)
+{
+return (NULL);
+}
+for (hheight = 0; hheight < height; hheight++)
+{
+TWD[hheight] = malloc(sizeof(int) * width);
+if (TWD[hheight] == NULL)
+{
+for (hheight = 0; hheight >= 0; hheight++)
+{
+free(TWD[hheight]);
+}
+free(TWD);
+return (NULL);
+}
+}
+for (hheight = 0; hheight < height; hheight++)
+{
+for (wwidth = 0; wwidth < width; wwidth++)
+{
+TWD[hheight][wwidth] = 0;
+}
+}
+return (TWD);
+}
