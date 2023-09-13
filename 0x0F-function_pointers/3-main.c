734b360495980504845fb4b63d730ee7eafceb91
Nada <nadafarrag619@gmail.com>
2023-09-13 21:18:47 +0300

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+#include "function_pointers.h"
+/**
+*main-prints the result of operations
+*@argc:The number of arguments
+*@@argv:array of pointer to the arguments that given to the system program
+*Return: Always 0 (success)
+*/
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+char *op;
+int n1;
+int n2;
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+n1 = atoi(argv[1]);
+n2 = atoi(argv[3]);
+op = argv[2];
+if (get_op_func(op) == NULL || op[1] != '\0')
+{
+printf("Error\n");
+exit(99);
+}
+if ((*op == '/' && n2 == 0) || (*op == '%' && n2 == 0))
+{
+printf("Error\n");
+exit(100);
+}
+printf("%d\n", get_op_func(op)(n1, n2));
+return (0);
+}
