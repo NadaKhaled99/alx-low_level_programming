0cf87f6f6c753c6530b2fbb507028b4623850723
Nada <nadafarrag619@gmail.com>
2023-09-13 21:28:09 +0300

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,8 +11,8 @@
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 char *op;
-int n1;
-int n2;
+int num1;
+int num2;
 if (argc != 4)
 {
 printf("Error\n");
@@ -26,11 +26,11 @@ if (get_op_func(op) == NULL || op[1] != '\0')
 printf("Error\n");
 exit(99);
 }
-if ((*op == '/' && n2 == 0) || (*op == '%' && n2 == 0))
+if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 {
 printf("Error\n");
 exit(100);
 }
-printf("%d\n", get_op_func(op)(n1, n2));
+printf("%d\n", get_op_func(op)(num1, num2));
 return (0);
 }
