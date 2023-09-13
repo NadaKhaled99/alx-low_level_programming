19c1e60e861c1e3dfacc23f8652f01e53325b0f1
Nada <nadafarrag619@gmail.com>
2023-09-13 19:14:19 +0300

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+*array_iterator-function that searches for an integer
+*@array:array of elements
+*@size:is the number of elements in the array
+*@cmp:ptr to function
+*Return:returns the index of the first element
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int j;
+if (array == NULL || size <= 0 || cmp == NULL)
+{
+return (-1);
+}
+for (j = 0; j < size; j++)
+{
+if (cmp(array[j]))
+{
+return (j);
+}
+}
+return (-1);
+}
