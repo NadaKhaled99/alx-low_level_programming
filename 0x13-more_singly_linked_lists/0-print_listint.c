cd48753af0ca96240bceedecdabcb23f9e7d2348
Nada <nadafarrag619@gmail.com>
2023-09-26 00:52:21 +0300

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,12 +6,12 @@
  */
 size_t print_listint(const listint_t *h)
 {
-size_t number = 0;
+size_t counter = 0;
 while (h)
 {
 printf("%d\n", h->n);
-number++;
+counter++;
 h = h->next;
 }
-return (number);
+return (counter);
 }
