733ae4e8670abec40d82547cf74bcdc940b55172
Nada <nadafarrag619@gmail.com>
2023-09-26 21:32:36 +0300

    101-print_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include "loop.h"
+/**
+*print_listint_safe-prints a listint_t linked list
+*@head:1st node in linked list
+*Return:the number of nodes in the list
+*/
+size_t print_listint_safe(const listint_t *head)
+{
+size_t nnodes = 0;
+size_t index = 0;
+nnodes = loop_listint(head);
+if (nnodes == 0)
+{
+for (; head != NULL; nnodes++)
+{
+printf("[%p] %d\n", (void *)head, head->n);
+head = head->next;
+}
+}
+else
+{
+for (index = 0; index < nodes; index++)
+{
+printf("[%p] %d\n", (void *)head, head->n);
+head = head->next;
+}
+printf("-> [%p] %d\n", (void *)head, head->n);
+}
+return (nnodes);
+}
---
+++ 0x13-more_singly_linked_lists/loop.h
@@ -0,0 +1,41 @@
+#include "lists.h"
+/**
+*looped_listint_len-Counts the number of unique nodes
+*@head:1st node in linked list
+*Return:the number of nodes in the list
+*/
+size_t loop_listint(const listint_t *head)
+{
+const listint_t *t;
+const listint_t *here;
+size_t nnodes = 1;
+if (head == NULL || head->next == NULL)
+{
+return (0);
+}
+t = head->next;
+here = (head->next)->next;
+while (here)
+{
+if (t == here)
+{
+t = head;
+while (tortoise != hare)
+{
+nnodes++;
+t = t->next;
+here = here->next;
+}
+t = t->next;
+while (t != here)
+{
+nnodes++;
+t = t->next;
+}
+return (nnodes);
+}
+t = t->next;
+here = (here->next)->next;
+}
+return (0);
+}
