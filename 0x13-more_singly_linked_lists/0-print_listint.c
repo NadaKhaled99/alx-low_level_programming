0bc2adec0367c8039621c7a4bea34d985e56cd7e
Nada <nadafarrag619@gmail.com>
2023-09-26 17:03:16 +0300

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,9 +1,11 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
 *print_listint-prints all the elements of a listint_t list
 *@h:ptr to the  list _t which can allowed to print
 *Return:number of nodes
- */
+*/
 size_t print_listint(const listint_t *h)
 {
 size_t counter = 0;
