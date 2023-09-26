d214d73b15c30b87f8ef87cf0a2d18ce1d9b9362
Nada <nadafarrag619@gmail.com>
2023-09-26 22:25:48 +0300

    102-free_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+*free_listint_safe-frees a listint_t list
+*@h:ptr to list_t list
+*Return:the size of the list that was free’d
+*/
+size_t free_listint_safe(listint_t **h)
+{
+size_t l = 0;
+int d;
+listint_t *tmp;
+if (!h || !*h)
+return (0);
+while (*h)
+{
+d = *h - (*h)->next;
+if (d > 0)
+{
+tmp = (*h)->next;
+free(*h);
+*h = tmp;
+l++;
+}
+else
+{
+free(*h);
+*h = NULL;
+l++;
+break;
+}
+}
+*h = NULL;
+return (l);
+}
