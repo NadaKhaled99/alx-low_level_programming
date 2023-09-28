0e765b652cb243e7842d76932b0a741283facd0e
Nada <nadafarrag619@gmail.com>
2023-09-29 02:22:57 +0300

    5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*flip_bits- returns the number of bits you would
+*@n:numper input
+*@m:number input
+*Return:no. of bit
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+int sum = 0;
+int z;
+unsigned long int c;
+unsigned long int d;
+d = n ^ m;
+for (z = 63; z >= 0; z--)
+{
+c = d >> z;
+if (c & 1)
+{
+sum++;
+}
+}
+}
