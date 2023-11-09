cdef03981fe014fc575d4cb1255be0ec9ea71bc2
Nada <nadafarrag619@gmail.com>
2023-11-10 01:06:32 +0200

    file

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,9 +1,8 @@
 #include "lists.h"
-
 /**
- * dlistint_len - returns the number of elements in a linked dlistint_t list.
- * @h: pointer to the list.
- * Return: number of nodes.
+*dlistint_len-the number of elements in a linked dlistint_t list
+*@h:ptr to the list
+*Return:number of nodes
  **/
 size_t dlistint_len(const dlistint_t *h)
 {
