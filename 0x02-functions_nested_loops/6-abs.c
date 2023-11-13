8bbcd624e7df88282fd4c25140d8d74dddb26df7
Nada <nadafarrag619@gmail.com>
2023-11-13 12:37:52 +0200

    file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+/**
+*_abs-compute the absolute value of an integer
+*@n:integer to be computed
+*Return:return absolute value of the integer
+*/
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+else
+{
+return (-n);
+}
+}
